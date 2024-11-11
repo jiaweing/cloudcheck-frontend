f9a331577f7c547c8adeaf9527e155db53fc3ea9
hey@jiaweing.com <hey@jiaweing.com>
2024-11-11 12:38:10 +0800

    feat: Removed slide number from pitch deck

--- src/app/pitch/page.tsx
+++ src/app/pitch/page.tsx
@@ -209,17 +209,19 @@ export default function PitchDeck() {
             </div>
           </CarouselItem>
         </CarouselContent>
-        <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2">
+        {/* <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2">
           <div className="flex flex-row items-center justify-center">
             <ThemeToggle />
           </div>
-        </div>
+        </div> */}
         <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
           <div className="flex flex-row items-center justify-center">
             <CarouselPrevious className="text-foreground" />
-            <div className="flex flex-col gap-2 items-center py-2 px-4 bg-muted/50 rounded-full text-sm text-foreground">
+            <div className="flex flex-col gap-2 items-center rounded-full text-sm text-foreground">
+              {/* <div className="flex flex-col gap-2 items-center py-2 px-4 bg-muted/50 rounded-full text-sm text-foreground"> */}
               <div className="flex flex-col gap-1">
-                Slide {current} of {count}
+                <ThemeToggle />
+                {/* Slide {current} of {count} */}
                 {/* <Progress value={(current / count) * 100} className="h-1" /> */}
               </div>
             </div>
